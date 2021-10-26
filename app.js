c19430b0ba137b389b16e57a6a0fc814c7eb0975
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2021-10-26 13:45:31 -0700

    cleans up all functions to have proper indentation and syntax, and confirms html pulls functions and operates correctly

--- app.js
+++ app.js
@@ -16,84 +16,86 @@
 //     default:
 //         console.log('Sorry, we are out of ' + expr + '.');
 // }
+let username;
+
 function userName() {
-    let username = prompt("What is your name?");   
+    username = prompt("What is your name?");   
     console.log(username + "! Welcome to the site!"); 
 
-    alert("Hello " + username + ", welcome to my site.")
+    alert("Hello " + username + ", welcome to my site.");
 }
 
 
 function colorAnswer() {
-    let userAnswer = prompt("Is Daniel's favorite color blue?") 
+    let userAnswer = prompt("Is Daniel's favorite color blue?","please type either 'yes' or 'no'") 
     let answerColor = userAnswer.toLowerCase();
 
     if(answerColor === "yes") {
         alert("nice.");
-        console.log("nice, his favorite color IS blue!")
+        console.log("nice, his favorite color IS blue!");
     }
     else {
         alert("you tried..");
-        console.log("feels bad, you'll get the next one.")
+        console.log("feels bad, you'll get the next one.");
     }
 }
 
 
 function dogAnswer() {
-    let userAnswer = prompt("Is Daniel's dog named Bleu?") 
+    let userAnswer = prompt("Is Daniel's dog named Bleu?","please type either 'yes' or 'no'");
     let answerDog = userAnswer.toLowerCase();
 
     if(answerDog === "no") {
         alert("good stuff, his dog's name is Clark.");
-        console.log("good stuff, his dog's name is Clark")
+        console.log("good stuff, his dog's name is Clark");
     }
     else {
         alert("you tried..");
-        console.log("feels bad, you'll get the next one.")
+        console.log("feels bad, you'll get the next one.");
     }
 }
 
 
 function catAnswer() {
-    let userAnswer = prompt("Is Daniel's cat named Archie?") 
+    let userAnswer = prompt("Is Daniel's cat named Archie?","please type either 'yes' or 'no'"); 
     let answerCat = userAnswer.toLowerCase();
 
     if(answerCat === "yes") {
         alert("good stuff, his cat's name is Archie.");
-        console.log("good stuff, his cat's name is Archie")
+        console.log("good stuff, his cat's name is Archie");
     }
     else {
         alert("you tried..");
-        console.log("feels bad, you'll get the next one.")
+        console.log("feels bad, you'll get the next one.");
     }
 }
 
 
 function carAnswer() {
-    let userAnswer = prompt("Is Daniel's car a Tacoma?") 
+    let userAnswer = prompt("Is Daniel's car a Tacoma?","please type either 'yes' or 'no'"); 
     let answerCar = userAnswer.toLowerCase();
 
     if(answerCar === "yes") {
         alert("good stuff, his car is a Tacoma, and he will not shut up about it!");
-        console.log("good stuff, his car is a Tacoma, and he will not shut up about it!")
+        console.log("good stuff, his car is a Tacoma, and he will not shut up about it!");
     }
     else {
         alert("you tried..");
-        console.log("feels bad, you'll get the next one.")
+        console.log("feels bad, you'll get the next one.");
     }
 }
 
 
 function foodAnswer() {
-    let userAnswer = prompt("Is Daniel's favorite food pizza?") 
+    let userAnswer = prompt("Is Daniel's favorite food pizza?","please type either 'yes' or 'no'"); 
     let answerFood = userAnswer.toLowerCase();
 
     if(answerFood === "no") {
-        alert("good stuff, his favorite food is actually burritos, yes plural.");
-        console.log("good stuff, his favorite food is actually burritos, yes plural")
+        alert("good stuff, his favorite food is actually burritos, yes plural. Thank you for playing " + username + "!");
+        console.log("good stuff, his favorite food is actually burritos, yes plural. Thank you for playing " + username + "!");
     }
     else {
-        alert("you tried..");
-        console.log("feels bad, you'll get the next one.")
+        alert("you tried.. but have a good day " + username + "!");
+        console.log("feels bad, but have a good day " + username + "!");
     }
 }
