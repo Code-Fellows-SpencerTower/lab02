c46aacdd76ca5e9ac9ba4e6e5a7d74b9e8326aa6
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2021-10-26 12:44:08 -0700

    adds initial functions for prompt questions

--- app.js
+++ app.js
@@ -1,34 +1,57 @@
 'use strict';
-/*
 
-let userResponse = prompt("Pick a number");
+// let expr = prompt("Choose a fruit, oranges, mangoes, or papayas");
+// let exprReg = expr.toLowerCase();
 
-let numberPicked = parseInt(userResponse);
+// switch (exprReg) {
+//     case 'oranges':
+//         console.log('Oranges are $0.59 a pound');
+//         break;
+//     case 'mangoes':
+//         console.log('Got Mangoes');
+//         break;
+//     case 'papayas':
+//         console.log('Papayas are 2.79 a pound');
+//         break;
+//     default:
+//         console.log('Sorry, we are out of ' + expr + '.');
+// }
+let userAnswer = prompt("Is Daniel's favorite color blue?") 
+let answerColor = userAnswer.toLowerCase();
 
-// we want to say Congrats when correct number (42) is entered
-// otherwise we say sorry
-
-if(numberPicked === 42) {
-    alert("Congrats");
-} else {
-    alert("sorry");
+if(answerColor === "yes") {
+    alert("nice.");
+}
+else {
+    alert("you tried..");
 }
 
-*/
-
-let expr = prompt("Choose a fruit, oranges, mangoes, or papayas");
-let exprReg = expr.toLowerCase();
-
-switch (exprReg) {
-    case 'oranges':
-        console.log('Oranges are $0.59 a pound');
-        break;
-    case 'mangoes':
-        console.log('Got Mangoes');
-        break;
-    case 'papayas':
-        console.log('Papayas are 2.79 a pound');
-        break;
-    default:
-        console.log('Sorry, we are out of ' + expr + '.');
+if(answerColor === "yes") {
+    alert("nice.");
 }
+else {
+    alert("you tried..");
+}
+
+if(answerColor === "yes") {
+    alert("nice.");
+}
+
+else {
+    alert("you tried..");
+}
+
+if(answerColor === "yes") {
+    alert("nice.");
+}
+else {
+    alert("you tried..");
+}
+
+if(answerColor === "yes") {
+    alert("nice.");
+}
+else {
+    alert("you tried..");
+}
+
