be5e16c57c50a3dedf60eb197c2cada1c6b3465e
Daniel Jackson <daniel.jakob.jackson@gmail.com>
2021-10-26 11:24:08 -0700

    moved filed from local folder to GitHub repo

---
+++ app.js
@@ -0,0 +1,9 @@
+let someData = 4;
+
+let resultFromTypeOf = typeof(someData);
+
+console.log('resultFromTypeOf = ', resultFromTypeOf);
+
+let doubled = someData + someData;
+
+console.log('doubled', doubled);
